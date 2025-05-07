70c4235b1699c45b0aac350cf1237999016078f2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-07 20:36:14 -0300

    commit

--- PrimeiroSemestre/src/FilasEncadeadas/FilaInt.java
+++ PrimeiroSemestre/src/FilasEncadeadas/FilaInt.java
@@ -1,2 +1,44 @@
-package FilasEncadeadas;public class FilaInt {
+package FilasEncadeadas;
+
+public class FilaInt {
+    private class NO{
+        int dado;
+        NO prox;
+    }
+
+    private NO ini,fim;
+
+    public void init(){
+        ini = fim = null;
+    }
+
+    public boolean isEmpty(){
+        return ini == null && fim == null;
+    }
+
+    public void enqueue(int elem){
+        NO novo = new NO();
+        novo.dado = elem;
+        novo.prox = null;
+        if(isEmpty()){
+            ini = novo;
+        }else{
+            fim.prox = novo;
+        }
+        fim = novo;
+    }
+
+    public int dequeue(){
+        int valor = ini.dado;
+        ini = ini.prox;
+        if(ini == null){
+            fim = null;
+        }
+        return valor;
+    }
+
+    public int firat(){
+        return ini.dado;
+    }
+
 }
--- PrimeiroSemestre/src/FilasEncadeadas/Main.java
+++ PrimeiroSemestre/src/FilasEncadeadas/Main.java
@@ -1,2 +1,24 @@
-package FilasEncadeadas;public class Main {
+package FilasEncadeadas;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        FilaInt fila = new FilaInt();
+        fila.init();
+        System.out.println("Informe valor positivo para inserir na fila: ");
+        int valor = sc.nextInt();
+        while(valor >= 0){
+            fila.enqueue(valor);
+            System.out.println("Informe valor positivo para inserir na fila: ");
+            valor = sc.nextInt();
+        }
+        System.out.println("Esvaziando a fila");
+        while (!fila.isEmpty()){
+            System.out.println("Retirando valor: " + fila.dequeue());
+        }
+
+        sc.close();
+    }
 }
